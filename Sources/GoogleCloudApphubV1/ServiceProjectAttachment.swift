978af0d4226e10878a061595d1338bafafb24e4a
Carlos O'Ryan <coryan@google.com>
2026-07-27 18:20:35 -0400

    chore: update librarian circa 2026-07-27 (#48)
    In this version, librarian generates correct cross-reference links for
    enums. This fixes a number of documentation warnings for
    `google-cloud-wkt` and many generated libraries.

--- Sources/GoogleCloudApphubV1/ServiceProjectAttachment.swift
+++ Sources/GoogleCloudApphubV1/ServiceProjectAttachment.swift
@@ -119,7 +119,7 @@ public struct ServiceProjectAttachment: Codable, Equatable, GoogleCloudWkt._AnyP
 
     /// Initialize from a string value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownStringValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownStringValue`](doc:State/unknownStringValue(_:)).
     public init(stringValue: Swift.String) {
       switch stringValue {
       case "STATE_UNSPECIFIED": self = .unspecified
@@ -132,7 +132,7 @@ public struct ServiceProjectAttachment: Codable, Equatable, GoogleCloudWkt._AnyP
 
     /// Initialize from an integer value.
     ///
-    /// If the value is unknown, this initializes to ``.unknownIntValue(_:)``.
+    /// If the value is unknown, this initializes to [`unknownIntValue`](doc:State/unknownIntValue(_:)).
     public init(intValue: Int) {
       switch intValue {
       case 0: self = .unspecified
